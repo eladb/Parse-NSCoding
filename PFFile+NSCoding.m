439db62411a750e7e61d9c634a31ff4f5d8fb81e
Martin Rybak <martin.rybak@gmail.com>
2014-02-04 21:49:58 -0500

    Updated PFFile serialization to include ivars and underlying NSData

--- PFFile+NSCoding.m
+++ PFFile+NSCoding.m
@@ -7,28 +7,70 @@
 //
 
 #import "PFFile+NSCoding.h"
+#import <objc/runtime.h>
 
 #define kPFFileName @"_name"
-#define kPFFileUrl @"_url"
+#define kPFFileIvars @"ivars"
+#define kPFFileData @"data"
 
 @implementation PFFile (NSCoding)
 
 - (void)encodeWithCoder:(NSCoder*)encoder
 {
 	[encoder encodeObject:self.name forKey:kPFFileName];
-	[encoder encodeObject:self.url forKey:kPFFileUrl];
+	[encoder encodeObject:[self ivars] forKey:kPFFileIvars];
+	if (self.isDataAvailable) {
+		[encoder encodeObject:[self getData] forKey:kPFFileData];
+	}
 }
 
 - (id)initWithCoder:(NSCoder*)aDecoder
 {
 	NSString* name = [aDecoder decodeObjectForKey:kPFFileName];
-    NSString* url = [aDecoder decodeObjectForKey:kPFFileUrl];
-	self = [super init];
+	NSDictionary* ivars = [aDecoder decodeObjectForKey:kPFFileIvars];
+	NSData* data = [aDecoder decodeObjectForKey:kPFFileData];
+	
+	self = [PFFile fileWithName:name data:data];
 	if (self) {
-		[self setValue:name forKey:kPFFileName];
-		[self setValue:url forKey:kPFFileUrl];
+		for (NSString* key in [ivars allKeys]) {
+			[self setValue:ivars[key] forKey:key];
+		}
 	}
 	return self;
+}
+
+- (NSDictionary*)ivars
+{
+    NSMutableDictionary* ivarsDict = [NSMutableDictionary new];
+    unsigned int count;
+    Ivar* ivars = class_copyIvarList([self class], &count);
+    for (int i = 0; i < count; i++)
+    {
+        Ivar ivar = ivars[i];
+        const char* name = ivar_getName(ivar);
+        const char* type = ivar_getTypeEncoding(ivar);
+        ptrdiff_t offset = ivar_getOffset(ivar);
+		
+		id value;
+		if (strncmp(type, "i", 1) == 0) {
+            int intValue = *(int*)((uintptr_t)self + offset);
+			value = @(intValue);
+            NSLog(@"%s = %i", name, intValue);
+        } else if (strncmp(type, "f", 1) == 0) {
+            float floatValue = *(float*)((uintptr_t)self + offset);
+			value = @(floatValue);
+            NSLog(@"%s = %f", name, floatValue);
+        } else if (strncmp(type, "@", 1) == 0) {
+            value = object_getIvar(self, ivar);
+            NSLog(@"%s = %@", name, value);
+        }
+		
+		if (value) {
+			[ivarsDict setObject:value forKey:[NSString stringWithFormat: @"%s", name]];
+		}
+    }
+    free(ivars);
+    return ivarsDict;
 }
 
 @end

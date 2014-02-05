da316623f9818876037e40555ae6250573c7dd38
Martin Rybak <martin.rybak@gmail.com>
2014-02-05 08:02:12 -0500

    Now using DHCObjectIntrospection to get ivars

--- PFFile+NSCoding.m
+++ PFFile+NSCoding.m
@@ -7,7 +7,7 @@
 //
 
 #import "PFFile+NSCoding.h"
-#import <objc/runtime.h>
+#import "NSObject+introspect.h"
 
 #define kPFFileName @"_name"
 #define kPFFileIvars @"ivars"
@@ -18,7 +18,7 @@
 - (void)encodeWithCoder:(NSCoder*)encoder
 {
 	[encoder encodeObject:self.name forKey:kPFFileName];
-	[encoder encodeObject:[self ivars] forKey:kPFFileIvars];
+	[encoder encodeObject:[self iVarsDict] forKey:kPFFileIvars];
 	if (self.isDataAvailable) {
 		[encoder encodeObject:[self getData] forKey:kPFFileData];
 	}
@@ -37,40 +37,6 @@
 		}
 	}
 	return self;
-}
-
-- (NSDictionary*)ivars
-{
-    NSMutableDictionary* ivarsDict = [NSMutableDictionary new];
-    unsigned int count;
-    Ivar* ivars = class_copyIvarList([self class], &count);
-    for (int i = 0; i < count; i++)
-    {
-        Ivar ivar = ivars[i];
-        const char* name = ivar_getName(ivar);
-        const char* type = ivar_getTypeEncoding(ivar);
-        ptrdiff_t offset = ivar_getOffset(ivar);
-		
-		id value;
-		if (strncmp(type, "i", 1) == 0) {
-            int intValue = *(int*)((uintptr_t)self + offset);
-			value = @(intValue);
-            NSLog(@"%s = %i", name, intValue);
-        } else if (strncmp(type, "f", 1) == 0) {
-            float floatValue = *(float*)((uintptr_t)self + offset);
-			value = @(floatValue);
-            NSLog(@"%s = %f", name, floatValue);
-        } else if (strncmp(type, "@", 1) == 0) {
-            value = object_getIvar(self, ivar);
-            NSLog(@"%s = %@", name, value);
-        }
-		
-		if (value) {
-			[ivarsDict setObject:value forKey:[NSString stringWithFormat: @"%s", name]];
-		}
-    }
-    free(ivars);
-    return ivarsDict;
 }
 
 @end

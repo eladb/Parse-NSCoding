d24a01ad4c13ed2eba504e1bc6560738bd7a93f9
alexander_zhigulich <alexander_zhigulich@cedoni.com>
2015-03-24 19:24:30 +0300

    Due to recent changes in Parse SDK PFFile cache will not work.

--- PFFile+NSCoding.m
+++ PFFile+NSCoding.m
@@ -17,30 +17,24 @@
 
 - (void)encodeWithCoder:(NSCoder*)encoder
 {
-	[encoder encodeObject:self.name forKey:kPFFileName];
+    [encoder encodeObject:self.name forKey:kPFFileName];
     [encoder encodeObject:self.url forKey:kPFFileURL];
-	if (self.isDataAvailable) {
-		[encoder encodeObject:[self getData] forKey:kPFFileData];
-	}
+    if (self.isDataAvailable) {
+        [encoder encodeObject:[self getData] forKey:kPFFileData];
+    }
 }
 
 - (id)initWithCoder:(NSCoder*)aDecoder
 {
-	NSString* name = [aDecoder decodeObjectForKey:kPFFileName];
+    NSString* name = [aDecoder decodeObjectForKey:kPFFileName];
     NSString* url = [aDecoder decodeObjectForKey:kPFFileURL];
-	NSData* data = [aDecoder decodeObjectForKey:kPFFileData];
-	
-    if (data) {
-        self = [PFFile fileWithName:name data:data];
-    } else {
-        self = [PFFile new];
-        [self setValue:name forKey:kPFFileName];
-        [self setValue:url forKey:kPFFileURL];
+    NSData* data = [aDecoder decodeObjectForKey:kPFFileData];
+    
+    self = [PFFile fileWithName:name data:data];
+    if (self) {
+        [self setValue:url forKey:@"_url"];
     }
-	if (self) {
-        [self setValue:url forKey:kPFFileURL];
-	}
-	return self;
+    return self;
 }
 
 @end
